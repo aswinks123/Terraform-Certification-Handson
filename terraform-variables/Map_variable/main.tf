83f465ebfe072d92d3b11b2f84ca67605406e3d7
aswin <aswinks123>
2024-08-15 15:44:46 -0400

    Map variable added

--- terraform-variables/Map_variable/main.tf
+++ terraform-variables/Map_variable/main.tf
@@ -1,3 +1,10 @@
+# =============================================================================
+# DEVELOPER: Aswin KS
+# PURPOSE: Demonstrate MAP variable in Terraform
+# DATE: 15-08-2024
+# =============================================================================
+
+
 #Specify the Provider block. Here we use Docker
 
 terraform {
@@ -16,7 +23,7 @@ resource "docker_image" "nginx" {
 }
 
 #------------------------------------------------------------------VARIABLE BLOCK END-------------------------------
-#Using variable to provide internal and external port
+#Using basic variable to provide container name and port
 
 variable "internal_port" {
     description = "Internal port of the container"
@@ -29,80 +36,55 @@ variable "external_port"{
     default = 5000
 }
 
-variable "container_name" {
-    description = "Name of the container"
-    type = string
-    default = "terraform-nginx"
-}
-
 #Variable as list of string - passing envs to container
 
 variable "container_names" {
   description = "A list of names for the containers"
   type        = list(string)
-  default     = ["container-1", "container-2"]
+  default     = ["container-1", "container-2", "container-3"]
 }
 
 
-#MAP variable that uses the CONDITIONAL EXPRESSION
+#MAP variable
+# MAP variable to inject multiple ENV inside the container
 
-# Conditional expression inside the env_variables map
 variable "env_variables" {
-  description = "Environment variables for the container that uses conditional expression"
+  description = "Environment variables for the container"
   type        = map(string)
-  default     = {
-    #if var.is_production  flag is true the it adds production else development    
-    "ENV"   = var.is_production ? "production" : "development"
+  default     = {    
+    "ENV"   = "Production"
     "OWNER" = "Aswin"
     "TOPIC" = "Terraform"
   }
 }
 
-#CONDITIONAL EXPRESSION
-# Variable to determine if the environment is production
-
-variable "is_production" {
-  description = "Boolean flag to indicate if the environment is production"
-  type        = bool
-  default     = true
-}
-
-
-
-
 #------------------------------------------------------------------VARIABLE BLOCK END-------------------------------
 
 #------------------------------------------------------------------CODE BLOCK START-------------------------------
-
-
 # Start multiple nginx container
-
-
 resource "docker_container" "nginx" {
 
+  #To use the list variable to create multiple containers
   count = length(var.container_names)
   name  = var.container_names[count.index]
   image = docker_image.nginx.image_id
 
-#Map the host port 5000 to container port 80
-   ports {
+#Map the host port variable to container port variable. Here we uses count.index to increment the external port 
+
+ ports {
     internal = var.internal_port
     external = var.external_port + count.index
   }
-
-#Refering the MAP variable to inject the env variables
-    env = [for k, v in var.env_variables : "${k}=${v}"]
-
 }
-
 
 #------------------------------------------------------------------CODE BLOCK END-------------------------------
 
 #------------------------------------------------------------------OUTPUT BLOCK START-------------------------------
 
+#we use  [*].id to refer elements of list
 output "container_ids" {
   description = "The IDs of the created containers"
-  value       = docker_container.nginx[*].id
+  value       = docker_container.nginx[*].id   
 }
 
 output "container_names" {
